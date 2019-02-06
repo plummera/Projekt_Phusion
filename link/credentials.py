58a279591019d2f7f2529e70297cf695d3116268
Anthony Plummer <aplummer@psda.com>
2019-02-06 08:39:50 -0500

    Blank Credentials uploaded

---
+++ link/credentials.py
@@ -0,0 +1,11 @@
+#The Twitter API credentials
+twitter_consumer_key = ''
+twitter_consumer_secret = ''
+twitter_access_token = ''
+twitter_access_secret = ''
+
+#FB credentials
+facebook_client_ID = ''
+facebook_client_secret = ''
+
+graph_key = ''
